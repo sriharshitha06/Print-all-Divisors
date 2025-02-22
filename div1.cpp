65136ed88119772b2b87f7ed88e3b5a30fff7939
MANTHENA SRI HARSHITHA <manthenasiri23@gmail.com>
2025-02-23 00:09:54 +0530

    Update div1.cpp

--- div1.cpp
+++ div1.cpp
@@ -2,29 +2,33 @@
 #include <vector>
 #include <cmath>
 using namespace std;
+
+// Find divisors of n
 vector<int> findDivisors(int n) {
-    vector<int> divisors; 
-    int sqrtN = sqrt(n); 
-    for (int i = 1; i <= sqrtN; ++i) { 
-        if (n % i == 0) { 
-            divisors.push_back(i); 
-            if (i != n / i) {
-                divisors.push_back(n / i); 
+    vector<int> divisors; // Store divisors
+    int sqrtN = sqrt(n); // Calculate square root of n
+
+    // Loop through numbers from 1 to sqrt(n)
+    for (int i = 1; i <= sqrtN; ++i) {
+        if (n % i == 0) { // If i is a divisor
+            divisors.push_back(i); // Add i to divisors
+            if (i != n / i) { // Check for distinct divisor
+                divisors.push_back(n / i); // Add paired divisor
             }
         }
     }
-    return divisors; 
+    return divisors; // Return list of divisors
 }
+
 int main() {
-    int num;
-  cin>>num;
-    vector<int> divisors = findDivisors(num);
-     cout << "Divisors of " << num << " are: ";
-    for (int divisor : divisors) {
-        cout << divisor << " ";
+    int num; // Input number
+    cin >> num; // Read input
+
+    vector<int> divisors = findDivisors(num); // Find divisors
+    cout << "Divisors of " << num << " are: "; // Output message
+    for (int divisor : divisors) { // Print each divisor
+        cout << divisor << " "; 
     }
-    cout << endl;
-    return 0;
+    cout << endl; 
+    return 0; 
 }
-                                
-                            
